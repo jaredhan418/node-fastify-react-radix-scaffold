6afbde61b17f7917de5d6aad4d70a3474f6750e8
jaredhan <hanjiawei@outlook.com>
2025-11-25 16:42:05 +0800

    chore: upgrade deps

--- schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE shortlink.short_origin_table (
-	shortlinkId varchar(8) NOT NULL,
-	originalLink TEXT NOT NULL,
-	createTime TIMESTAMP NOT NULL
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_general_ci;
