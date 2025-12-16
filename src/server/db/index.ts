eec3949bd22c62f5b768bc90d4b6c24cd94ad12c
jaredhan <hanjiawei@outlook.com>
2025-12-17 01:06:15 +0800

    chore: upgrade deps

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,3 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../../prisma/generated/prisma/client.js";
 
-export const prisma = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+export const prisma = new PrismaClient({ adapter });
